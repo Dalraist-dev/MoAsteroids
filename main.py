e8c5d2a0a6ead9d7cff86ec70ee04d9c3bce4f1a
Dalraist <moses.beuthin@gmail.com>
2025-07-27 21:47:42 -0500

    0.4 Added player.py, Updated main.py, constants.py, circleshape.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import pygame
 from pygame.locals import *
 from constants import *
+from player import Player
 
 # Main Functions & Methods
 
@@ -12,14 +13,19 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = game_clock.tick(60) / 1000
 
 
