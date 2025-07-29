298197fbf70e20f39394ccdc3531797d41ed6c06
Dalraist <moses.beuthin@gmail.com>
2025-07-28 21:44:57 -0500

    0.5 Movement & Group Update -  Updated main.py, constants.py, player.py

--- main.py
+++ main.py
@@ -10,19 +10,35 @@ from player import Player
 
 def main():
     
+    # Game Initiation Variables
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
+    # Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    # Player
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    # Main Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
+        # Sprite Updates
+        updatable.update(dt)
+
+        # Screen Refresh/Draw   
         screen.fill("black")
-        player.draw(screen)
+
+        for drawable_sprite in drawable:
+            drawable_sprite.draw(screen)
+
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
