9d3111600c3c2ce48cd567a3d6bb82b849587d44
Dalraist <moses.beuthin@gmail.com>
2025-07-31 08:03:22 -0500

    0.8 Shooting Update, Added shot.py, Updated contstants.py, main.py, player.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 # Main Functions & Methods
 
@@ -26,9 +27,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     #Containers
     Player.containers = (updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
 
